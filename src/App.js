fa5412c7c3223dec338de90a1073beac0520020d
Jasper-Rowan <jasper.rowan@berkeley.edu>
2022-12-23 16:05:10 -0800

    added a button to toggle if important notes or all notes would be shown

--- src/App.js
+++ src/App.js
@@ -77,17 +77,32 @@ const App = () => {
   
   const addNote = (event) => {
     event.preventDefault()
-    console.log('button clicked', event.target)
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+  
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
   }
+
+  const notesToShow = showAll
+    ? notes
+    : notes.filter(note => note.important === true)
   
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        {notes.map(note => 
+        {notesToShow.map(note => 
           <Note key={note.id} note={note} />
         )}
       </ul>
+      <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all' }
+        </button>
       <form onSubmit={addNote}>
         <input 
           value={newNote}
