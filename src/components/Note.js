c998737d1fe340fc17dbad2e85e6a2e01b762d1c
Jasper-Rowan <jasper.rowan@berkeley.edu>
2022-12-24 13:02:00 -0800

    added the ability for the importance of notes to be updated and created a seprate services directory where the logic of the server calls to get and update notes on the server would live in a notes.js file

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,3 +1,17 @@
+/**
+ * A Note which contains text content and a button to change
+ * its importance.
+ * 
+ * @param {note, toggleImportance} 
+ * 
+ * note is a object that has a minimum of and id,
+ * importance, and content.
+ * 
+ * toggleImportance is a reference to a callback function
+ * that will be called when the importance button is
+ * clicked.
+ * @returns a note to be rendered. 
+ */
 const Note = ({ note, toggleImportance }) => {
     const label = note.important
       ? 'make not important' : 'make important'
